2489311d9ad80fd5ca3b9d14b951919e32a80dd4
ferxas <ferxas321@hotmail.es>
2024-10-05 05:53:19 -0500

    ok, parece que olvidé quitarle algo de margen al footer xD

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
-        <footer className="bg-gray-800 text-white py-8 mt-5">
+        <footer className="bg-gray-800 text-white py-8">
             <div className="container mx-auto px-4">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                     <div>
